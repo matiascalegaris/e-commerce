c3b6ba1f1729b879979a173b3d3df07b9acafcea
Matias <calegaris.matias@gmail.com>
2021-12-06 12:34:10 -0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component'
 import { ShopPage } from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignup from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot( snapshot => {
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data()
-            }
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data()
           })
         });
       }
-      this.setState({currentUser:null});
+      else {
+        setCurrentUser(userAuth);
+      }
     });
   } 
 
@@ -42,7 +38,7 @@ class App extends React.Component {
   render() {
   return (
     <div>
-      <Header currentUser={this.state.currentUser} />
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage/>} />
         <Route exact path='/shop' element={<ShopPage/>} />
@@ -53,4 +49,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/redux/user/user.actions.js
@@ -0,0 +1,5 @@
+import { SET_CURRENT_USER } from "./user-reducer"
+export const setCurrentUser = user => ({
+  type: SET_CURRENT_USER,
+  payload: user
+})
